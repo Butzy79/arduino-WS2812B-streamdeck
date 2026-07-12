892c8bdd54b2c2aac7f4f5d23a971ba9de254278
butzy79 <butzy79@icloud.com>
2026-07-12 18:27:03 +0200

    sasa

---
+++ deploy.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal
+
+echo ==========================================
+echo Elgato Plugin
+echo ==========================================
+
+REM =====================================================
+REM PROJECT ROOT PATHS
+REM =====================================================
+
+set "ROOT=C:\Users\livet\Documents\arduino-WS2812B-streamdeck"
+set "ELGPLG=C:\Users\livet\AppData\Roaming\Elgato\StreamDeck\Plugins"
+
+REM =====================================================
+REM DEBUG (optional but useful)
+REM =====================================================
+
+echo ROOT = %ROOT%
+echo ELGPLG = %ELGPLG%
+echo.
+
+REM =====================================================
+REM COPY DDS FILES (INCLUDING SUBFOLDERS)
+REM =====================================================
+
+echo.
+echo Copying Plugins files for...
+
+robocopy "%ROOT%\serial-usb-commander2\com.butzy79.serial-usb-commander2.sdPlugin" "%ELGPLG%\\com.butzy79.serial-usb-commander2.sdPlugin" *.* /S /COPY:DAT /R:1 /W:1
+
+echo.
+
+REM =====================================================
+REM DONE
+REM =====================================================
+
+echo.
+echo ==========================================
+echo DEPLOY COMPLETED SUCCESSFULLY
+echo ==========================================
