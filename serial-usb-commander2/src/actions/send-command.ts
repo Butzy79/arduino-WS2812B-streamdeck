b02485eb8e4cd6e4764cc11385027e4d8b8b7cb5
butzy79 <butzy79@icloud.com>
2026-07-12 21:16:21 +0200

    ssss

--- serial-usb-commander2/src/actions/send-command.ts
+++ serial-usb-commander2/src/actions/send-command.ts
@@ -9,21 +9,6 @@ export class SendSerialCommand extends SingletonAction<SerialSettings> {
 
 		streamDeck.logger.info(`KEY PRESSED. Command to send: ${command}`);
 
-		const { SerialPort } = await import("serialport");
-
-		const port = new SerialPort({
-			path: "COM5",
-			baudRate: 115200
-		});
-
-		port.on("open", () => {
-			streamDeck.logger.info("SERIAL OPEN");
-		});
-
-		port.on("error", (err) => {
-			streamDeck.logger.error(`SERIAL ERROR: ${err.message}`);
-		});
-
 		await ev.action.showOk();
 	}
 }
