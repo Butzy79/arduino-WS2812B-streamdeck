9fbfd96d496141bbb85aca40d04b8449a889efec
butzy79 <butzy79@icloud.com>
2026-07-12 21:23:16 +0200

    com7

--- serial-usb-commander2/src/actions/send-command.ts
+++ serial-usb-commander2/src/actions/send-command.ts
@@ -6,7 +6,28 @@ export class SendSerialCommand extends SingletonAction<SerialSettings> {
 	override async onKeyDown(ev: KeyDownEvent<SerialSettings>): Promise<void> {
 		const command = ev.payload.settings.command ?? "ON";
 		streamDeck.logger.info(`KEY PRESSED -> Command to send: ${command}`);
+		const port = new SerialPort({
+			path: "COM7",
+			baudRate: 115200
+		});
 
+		port.on("open", () => {
+			streamDeck.logger.info("SERIAL OPEN");
+
+			port.write(command + "\n", (err) => {
+				if (err) {
+					streamDeck.logger.error("WRITE ERROR: " + err.message);
+				} else {
+					streamDeck.logger.info("SERIAL SENT: " + command);
+				}
+
+				port.close();
+			});
+		});
+
+		port.on("error", (err) => {
+			streamDeck.logger.error("SERIAL ERROR: " + err.message);
+		});
 		await ev.action.showOk();
 	}
 }
