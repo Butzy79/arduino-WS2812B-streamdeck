eefe2c748c4ef95d408e11222a0e7a3c8364541f
butzy79 <butzy79@icloud.com>
2026-07-12 22:27:48 +0200

    rolluop

--- serial-usb-commander2/src/actions/send-command.ts
+++ serial-usb-commander2/src/actions/send-command.ts
@@ -14,7 +14,7 @@ export class SendSerialCommand extends SingletonAction<SerialSettings> {
 
 		// 1. CREA LA PORTA CON AUTOOPEN DISABILITATO
 		const port = new SerialPort({
-			path: "COM7",
+			path: "COM5",
 			baudRate: 115200,
 			autoOpen: false // <--- FONDAMENTALE! Evita che parta prima di agganciare i listener
 		});
