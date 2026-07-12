39a6f484dda971dbd09a6a3180513b4f9cc15a0b
butzy79 <butzy79@icloud.com>
2026-07-12 20:26:44 +0200

    update log

--- serial-usb-commander2/src/plugin.ts
+++ serial-usb-commander2/src/plugin.ts
@@ -2,8 +2,14 @@ import streamDeck from "@elgato/streamdeck";
 
 import { SendSerialCommand } from "./actions/send-command";
 
+console.log("PLUGIN STARTED");
+
 streamDeck.logger.setLevel("trace");
 
 streamDeck.actions.registerAction(new SendSerialCommand());
 
+console.log("ACTION REGISTERED");
+
 streamDeck.connect();
+
+console.log("CONNECT CALLED");
